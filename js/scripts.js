c5c79dc5e90187d630efb64dca791fe04c1dfad2
nxlu <nxlu@pge.com>
2018-12-10 15:27:30 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
     function load()
     {
-    window.location.href = "https://www.openstreetmap.org/?mlon=22.5739625&mlat=57.6287332#map=11/57.6413/22.6284";
+    //window.location.href = "https://www.openstreetmap.org/?mlon=169.4203047&mlat=67.4113917";
+	
+	window.location.href = "https://www.google.com/maps/@67.4113917,169.4203047,3a,75y,353.08h,77.88t/data=!3m8!1e1!3m6!1sAF1QipPCMXGtiGV44ydbWKt_UXDPMzqL7-WWO5mUbxYA!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPCMXGtiGV44ydbWKt_UXDPMzqL7-WWO5mUbxYA%3Dw203-h100-k-no-pi0-ya0.50000393-ro-0-fo100!7i10240!8i5120"
 
     }
