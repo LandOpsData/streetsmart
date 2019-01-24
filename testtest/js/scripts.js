ee387b28372a5b6d588c2fbd2e2ccbb87712af00
nxlu <nxlu@pge.com>
2019-01-24 11:53:00 -0800

    upload did not work. truncation in process

--- testtest/js/scripts.js
+++
@@ -1,8 +0,0 @@
-    function load()
-    {
-    //window.location.href = "https://www.openstreetmap.org/?mlon=169.4203047&mlat=67.4113917";
-	
-	//window.location.href = "https://www.google.com/maps/@67.4113917,169.4203047,3a,75y,353.08h,77.88t/data=!3m8!1e1!3m6!1sAF1QipPCMXGtiGV44ydbWKt_UXDPMzqL7-WWO5mUbxYA!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPCMXGtiGV44ydbWKt_UXDPMzqL7-WWO5mUbxYA%3Dw203-h100-k-no-pi0-ya0.50000393-ro-0-fo100!7i10240!8i5120"
-
-	window.location.href = "https://www.google.com/maps/@45.9196976,101.7165253,3a,75y,205.52h,64.4t/data=!3m8!1e1!3m6!1sAF1QipP5A7yIKZoA1GvCxsrlJJgOA7w7kGWbrpIhTzI2!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipP5A7yIKZoA1GvCxsrlJJgOA7w7kGWbrpIhTzI2%3Dw203-h100-k-no-pi0-ya338.5-ro-0-fo100!7i8704!8i4352"
-    }
